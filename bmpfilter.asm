a3a509b8bea70dd08796a6cd0bb3073bc2600abd
piotrszmurlo <piotr.szmurlo7@gmail.com>
2021-11-14 22:57:06 +0100

    comments

--- bmpfilter.asm
+++ bmpfilter.asm
@@ -10,24 +10,27 @@ header:
 start_message: .asciiz "Input bmp file name: "
 read_error: .asciiz "File read failed"
 out_name: .asciiz "out.bmp"
-file_name: .ascii "bmptest.bmp"
+file_name: .ascii "monalisa.bmp"
 	.text
 	.globl main
 	
-#s0 = input file descriptor; s1 = output file descriptor; s3 = bitmap width; s4 = bitmap height; s5 = width + padding(bytes);
+#s0 = input file descriptor; s1 = output file descriptor; s3 = bitmap width; s4 = bitmap height; s5 = width + padding(bytes); s6 = file size in bytes (w/o header bytes)
 	
 main:
-	li $v0, 4		#welcome user
+#welcome user
+	li $v0, 4		
 	la $a0, start_message
 	syscall
 	
+#tbd	
 	#li $v0, 8
 	#la $a0, file_name	#input file name
 	#li $a1, 64	
 	#syscall
 	
+#open input file
 	li $v0, 13
-	la $a0, file_name	#open input file
+	la $a0, file_name	
 	li $a1, 0
 	li $a2, 0
 	syscall
@@ -35,6 +38,7 @@ main:
 	move $s0, $v0		#s0 = input file descriptor
 	blt $s0, $zero, file_read_error
 	
+#open outfile
 	li $v0, 13
 	la $a0, out_name	#create out.bmp
 	li $a1, 1
@@ -44,43 +48,56 @@ main:
 	move $s1, $v0		#s1 = output file descriptor
 	blt $s0, $zero, file_read_error
 	
+#read input file first two bytes
 	li $v0, 14
 	move $a0, $s0
-	la $a1, header		#read input file header
+	la $a1, header		
 	li $a2, 2	
 	syscall
 	
-	##test write first 2 bytes to file
+#write first 2 bytes to outfile
 	li $v0, 15
 	move $a0, $s1		#write header to outfile
 	la $a1, header	
 	li $a2, 2
 	syscall
-	##test
+	
+#read rest of 54-byte header
 	li $v0, 14
 	move $a0, $s0
 	la $a1, header		#read input file header
 	li $a2, 52	
 	syscall
-	##tset	
+	
+#write rest of 54-byte header to outfile
 	li $v0, 15
 	move $a0, $s1		#write header to outfile
 	la $a1, header	
 	li $a2, 52
 	syscall
 	
+#get bmp size and dimensions
 	lw $s6, header		# s6 = file size in bytes
 	addiu $s6, $s6, -54
 	lw $s3, header+16	# s3 = bitmap width
 	lw $s4, header+20	# s4 = bitmap height
 	
-	###read file size in bytes
+#temp###read file size in bytes
 	li $v0, 1
 	move $a0, $s6
 	syscall
-	###
 	
-	li $v0, 9 		#allocate buffer based on file siez
+	li $v0, 1
+	move $a0, $s3
+	syscall
+	
+	li $v0, 1
+	move $a0, $s4
+	syscall
+########
+
+#dynamically allocate file buffers based on bmp size
+	li $v0, 9 		#allocate buffer based on file size
 	move $a0, $s6		
 	syscall
 	sw $v0, file_buffer	#store pointer in file_buffer
@@ -89,22 +106,24 @@ main:
 	syscall
 	sw $v0, file_buffer_copy#store pointer in file_buffer_copy
 	
-	
+#remove possible minus from height
 	abs $s4, $s4		#left-right, top-bottom
 	
+#calculate padding and real byte width of bmp
 	mul $t0, $s3, 3		#width in bytes
 	andi $t1, $t0, 3	#(width in bytes) % 4
 	li $t2, 4
 	subu $t3, $t2, $t1	# t3 = padding
 	addu $s5, $t0, $t3	# s5 = width + padding(bytes)
 	
+#read input file pixel color bytes into file_buffer
 	li $v0, 14
 	move $a0, $s0
-	lw $a1, file_buffer	#read input file pixel bytes into file_buffer
+	lw $a1, file_buffer
 	move $a2, $s6
 	syscall
 
-
+#copy file buffer into copy buffer
 	li $t0, 0
 	lw $t1, file_buffer
 	lw $t2 file_buffer_copy
@@ -116,8 +135,17 @@ copy_buffer_loop:
 	addiu $t0, $t0, 4
 	blt $t0, $s6, copy_buffer_loop
 
+#######################################
 
 
+
+
+
+
+
+
+
+####################################### 
 
 write: 
 	li $v0, 15
--- median_filter.asm
+++ median_filter.asm
@@ -7,7 +7,9 @@ file_buffer_copy: .space 800000
 start_message: .asciiz "Input bmp file name: "
 read_error: .asciiz "File read failed"
 out_name: .asciiz "out.bmp"
+	.align 1
 header: .space 54
+	.align 2
 median_array: .space 25
 	.text
 	.globl main
@@ -53,8 +55,10 @@ main:
 	
 
 	
-	lw $s3, header+18	# s3 = bitmap width
-	lw $s4, header+22	# s4 = bitmap height
+	#lw $s3, header+18	# s3 = bitmap width
+	li $s3, 600
+	li $s4, 400
+	#lw $s4, header+22	# s4 = bitmap height
 	
 	abs $s4, $s4		#left-right, top-bottom
 	
@@ -75,17 +79,21 @@ main:
 	move $a0, $s0	#close file
 	syscall
 
-	li $t1, 0
-	li $t2, 0
+	li $t7, 0
+	li $t8, 0
 
-loop:
-	beq $t1, 240000, write
-	lb $t3, file_buffer($t2)
-	li $t3, 255
-	sb $t3, file_buffer($t2)
-	addiu $t2, $t2, 3
-	addiu $t1, $t1, 1
-	j loop
+loop_over_bmp:
+	#t9, - 3 t8 - 2,t7 - 1 
+	lb $t9, file_buffer($t8)
+	la $a0, file_buffer($t8)
+	jal load_med_array	#loading sorted array
+	beq $t7, 240000, write
+	#li $t3, 255
+	move $t9, $v0
+	sb $t9, file_buffer($t8)
+	addiu $t8, $t8, 3
+	addiu $t7, $t7, 1
+	j loop_over_bmp
 
 load_med_array:	#a0 - current pixel color address
 	lb $t0, ($a0)
@@ -117,13 +125,53 @@ loop_over_median_matrix:
 	
 	addu $a0, $a0, $s6		#a0 -> (0,x+1)
 	addiu $t3, $t3, 1
-	beq $t3, 5, sort_median_array
+	beq $t3, 5, print_arr
+	#beq $t3, 5, sort_array
 	j loop_over_median_matrix
 	
+###	
+	la $t0, median_array
+	li $t1, 0
+print_arr:
+	beq $t1, 100, exit
+	lw $a0, median_array($t1)
+	li $v0, 1
+	addiu $t1, $t1, 4
+	syscall
+	j print_arr
+###	
+sort_array:
+	la $t0, median_array
+	addiu $t0, $t0, 100 	#100 bytes a
+outer_loop:
+	li $t1, 0
+	la $a0, median_array
+inner_loop:
+	lw $t2, 0($a0)
+	lw $t3 4($a0)
+	slt $t5, $t2, $t3
+	beq $t5, $zero, continue
+	li $t1, 1
+	sw $t2, 4($a0)
+	sw $t3, 0($a0)
+continue:
+	addiu $a0, $a0, 4
+	bne $a0, $t0, inner_loop
+	bne $t1, $zero, outer_loop
+	li $t0, 20 	#array element offset (index)
+	li $t1 0	#sum
+calculate_avg:
+	beq $t0, 64, return_avg	#exit?
+	lw $t2, median_array($t0)	#load element
+	add $t1, $t1, $t2	#add element to sum
+	addiu $t0, $t0, 4
+	j calculate_avg
 
+return_avg:
+	divu $v0, $t1, 15	#return average
+	jr $ra
 	
-
-
+	
 write: li $v0, 15
 	move $a0, $s1		#write pixel bytes to outfiledddddddddd
 	la $a1, file_buffer	
